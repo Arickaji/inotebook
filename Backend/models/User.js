96c7933f194eb2d489ca28403bc37a5dcad302de
Aric kaji <arickaji007@gmail.com>
2022-04-05 16:03:01 +0530

    validation and thunderclient collections

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
 const UserSchema = new Schema({
-    name : {
+    name:{
         type: String,
         required: true
     },
-    email : {
-        type : String,
-        required : true
+    email:{
+        type: String,
+        required: true,
+        unique: true
     },
-    password : {
-        type : String,
-        required : true
+    password:{
+        type: String,
+        required: true
     },
-    date : {
-        type : Date,
-        default : Date.now
+    date:{
+        type: Date,
+        default: Date.now
     },
-});
-
-module.exports = mongoose.model('user',UserSchema)
+  });
+  const User = mongoose.model('user', UserSchema);
+  module.exports = User;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,39 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const router = express.Router(); 
+const { body, validationResult } = require('express-validator');
 
-// create a user using : POST "/api/auth".doesn't required auth
-router.post('/',(req,res)=>{
-    // console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
-})
 
-module.exports = router;
+// Create a User using: POST "/api/auth/". Doesn't require Auth 
+router.post('/createuser',[
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+] , async (req, res)=>{ 
+    // if their are errors return bad requests and error 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // check whether the user with email exits already
+    try{
+      let user = await User.findOne({email : req.body.email}); 
+      if(user){
+        return res.status(400).json({error : "Sorry a user with this emial already exits"})
+      }
+      //create user 
+      user = await User.create({
+          name: req.body.name,
+          password: req.body.password,
+          email: req.body.email,
+        })
+      res.json(user)
+    }
+    catch(error){
+      console.error(error.message);
+      res.status(500).send("some error occured")
+    }
+ 
+} )
+
+module.exports = router
