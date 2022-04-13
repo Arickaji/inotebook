fcd3cf8edadca8c81b0ab22b19f9f01d1ed19842
Aric kaji <arickaji007@gmail.com>
2022-04-13 15:52:23 +0530

    Add Update and Delete Features

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -49,4 +49,56 @@ router.post(
     }
   }
 );
+
+// router 3: update Note put : /api/auth/updateNote login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const {title,description,tag} = req.body;
+  try{
+    // create a new noe obj
+    const newNote = {};
+    if(title){newNote.title = title;}
+    if(description){newNote.description = description;}
+    if(tag){newNote.tag = tag;}
+  
+    // find the note to be updated and update it 
+    let note = await Notes.findById(req.params.id);
+    if(!note){
+      return res.status(404).send("Not Found");
+    }
+  
+    if(note.user.toString() != req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+  
+    note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new : true});
+    res.json(note);
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// router 4: delete Note delete : /api/auth/deleteNote login required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  // find the note and delete it
+  try{
+    let note = await Notes.findById(req.params.id);
+    if(!note){
+      return res.status(404).send("Not Found");
+    }
+    // allow deletion only if user owns this note
+    if(note.user.toString() != req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+  
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({"Success":"Note has been deleted",note : note});
+  }
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
